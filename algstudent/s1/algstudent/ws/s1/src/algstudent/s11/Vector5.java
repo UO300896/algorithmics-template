6e22058597a336c2114e8aa9453ffdb452df72f2
Alejandro de San Claudio Mesa <UO300896@uniovi.es>
2025-02-06 19:58:49 +0100

    s1

---
+++ algstudent/s1/algstudent/ws/s1/src/algstudent/s11/Vector5.java
@@ -0,0 +1,32 @@
+package algstudent.s11;
+
+/**
+ * This program serves to measure times automatically increasing 
+ * the size of the problem. In addition, we use a repetition value 
+ * determined by nTimes, an argument of the program
+ */
+public class Vector5 {
+	static int[]v;
+	static int[] m;
+	public static void main(String arg []) {
+		int repetitions = Integer.parseInt(arg[0]);
+		long t1,t2;
+	
+		
+		for (int n=10000; n<=Integer.MAX_VALUE; n*=2){ //n is increased *5   
+			  v = new int[n];
+			  Vector1.fillIn(v);	
+			  m = new int[2];
+			  t1 = System.currentTimeMillis();
+			  //We have to repeat the whole process to be measured
+			  for (int repetition=1; repetition<=repetitions; repetition++){    	
+			   Vector1.maximum(v, m);
+			   
+			  }
+			  t2 = System.currentTimeMillis();
+			  System.out.printf("SIZE=%d TIME=%d milliseconds Maximum=%d NTIMES=%d\n", n, t2-t1, m[1], repetitions);	
+		}//for 
+		
+	}//main
+
+}
